610a10bb649701228930c1d7c0ea5574e512bd9d
tjtjdnjswhd <tjtjdnjswhd@gmail.com>
2024-05-07 10:47:51 +0900

    :sparkles: MySql code first migration 추가

--- src/BlogPlatform.EFCore/Program.cs
+++ src/BlogPlatform.EFCore/Program.cs
@@ -5,13 +5,15 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+using MySqlConnector;
+
 var builder = Host.CreateApplicationBuilder(args);
 
-string connectionString = builder.Configuration.GetConnectionString("BlogPlatform") ?? throw new NullReferenceException();
+MySqlConnectionStringBuilder mySqlConnectionStringBuilder = builder.Configuration.GetRequiredSection("MySqlConnectionString").Get<MySqlConnectionStringBuilder>() ?? throw new Exception();
 
 builder.Services.AddDbContext<BlogPlatformDbContext>(options =>
 {
-    options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 36)));
+    options.UseMySql(mySqlConnectionStringBuilder.ToString(), new MySqlServerVersion(new Version(8, 0, 36)));
 });
 
 var app = builder.Build();
